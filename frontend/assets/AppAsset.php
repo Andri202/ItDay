3e5750b8fdc93641235bc195169b88adb84dfc54
Andri Gunawan <andrign20@gmail.com>
2018-02-14 12:09:18 +0700

    add frontend theme

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -9,12 +9,18 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    //public $basePath = '@webroot';
+    //public $baseUrl = '@web';
+    public $sourcePath = '@bower/debut/';
     public $css = [
-        'css/site.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css',
+        'scss/main.css',
+        'scss/skin.css',
     ];
     public $js = [
+        'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js',
+        'http://netdna.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js',
+        './script/index.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
